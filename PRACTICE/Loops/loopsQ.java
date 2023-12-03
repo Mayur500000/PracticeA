69b9f34b49e28899daf958d0e33404584d730691
Mayur500000 <142081670+Mayur500000@users.noreply.github.com>
2023-12-03 11:18:20 +0530

    loops

---
+++ PRACTICE/Loops/loopsQ.java
@@ -0,0 +1,46 @@
+package Loops;
+
+public class loopsQ {
+    
+//--------Qustion 13
+// public static int countN(int a){
+//     int count = 0;
+//      while(a!=0){
+     
+//       count ++;
+//        a/=10;
+//    }
+//     return count;
+// }
+// public static void main(String[] args) {
+//    int a= 153;
+//    int oga= 153;
+//    int count = countN(a);;
+ 
+//    int sum=0;
+ 
+  
+//    //System.out.println(a);
+//     while(a!=0){
+//     int d = a%10;
+//     int temp = 1;
+//     //System.out.println(d);
+//     for(int i = 1;i<=count;i++){
+//      temp =d * temp; 
+ 
+//    }
+//    sum +=temp; 
+    
+//    a/=10;
+// }
+// System.out.println(sum);
+// System.out.println(oga);
+// if(sum==oga){
+//     System.out.println("A");
+// }
+// else{
+//     System.out.println("NA");
+// } 
+// }
+
+}
